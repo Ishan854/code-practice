0adbf96b9e922c9d363e348847da4cdb268ffb6a
Kumar Ishan <50164669+Ishan854@users.noreply.github.com>
2025-10-12 18:07:17 +0000

    Constructor Java covered

---
+++ RectangleTest.java
@@ -0,0 +1,57 @@
+public class RectangleTest {
+
+    private double length;
+    private double breadth;
+    public RectangleTest(){
+        length  = 1;
+        breadth = 1;
+    }
+    public RectangleTest(double l, double b){
+       setLength(l);
+       setBreadth(b);
+    }
+    public RectangleTest(double s){
+        length = breadth = s;
+    }
+
+    public double getLength(){
+        return length;
+    }
+     public double getBreadth(){
+        return breadth;
+    }
+
+    public void setLength(double l){
+        if(l>=0){
+            length = l;
+        }else{
+            length = 0;
+        }
+    }
+     public void setBreadth(double b){
+        if(b>=0){
+            breadth = b;
+        }else{
+            breadth = 0;
+        }
+    }
+    public double area(){
+        return getLength()*getBreadth();
+    }
+    public double perimeter(){
+        return 2*(length+breadth);
+    }
+    public boolean isSquare(){
+        if(length==breadth){
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        RectangleTest r = new RectangleTest(10, 10);
+        System.out.println("Area: " + r.area());
+        System.out.println("Perimeter: " + r.perimeter());
+        System.out.println("Is Square? " + r.isSquare());
+    }
+}
