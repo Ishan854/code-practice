d8747b3707e8cd319d5a44aedcf2ec3541bdd1f8
Kumar Ishan <50164669+Ishan854@users.noreply.github.com>
2025-11-13 15:23:55 +0000

    Student Management System

---
+++ Main.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    private static List<Student> studentList = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Student s1 = new Student("Ishan", 25, "S-123");
+        s1.enrollCourse("Java");
+        System.out.println(s1);
+        s1.printStudentInfo();
+        System.out.println("--------X----------------X----------------X--------");
+        Student s2 = new Student("Rohan", 29, "S-136");
+        s2.enrollCourse("Python");
+        System.out.println(s2);
+        s2.printStudentInfo();
+
+        System.out.println("--------X----------------X----------------X--------");
+        Student s3 = new Student("Rajkumar", 29, "S-146");
+        s3.enrollCourse("DevOps");
+        System.out.println(s3);
+        s3.printStudentInfo();
+        studentList.add(s1);
+        studentList.add(s2);
+        studentList.add(s3);
+        System.out.println(studentList);
+
+        Student result = findStudentById("S-136");
+
+        System.out.println("Result: " + result);
+
+    }
+
+    public static Student findStudentById(String studentId){
+        Student result = null;
+        try{
+                result = studentList.stream().filter(x -> x.getStudentId().equalsIgnoreCase(studentId))
+                .findFirst()
+                .orElseThrow(()->  new RuntimeException("No Data Found!"));
+        }catch(RuntimeException r){
+            System.err.println("Student with " + studentId + "does not exist!!");
+        }
+        return result;
+    }
+}
---
+++ Student.java
@@ -0,0 +1,103 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Student {
+
+    private String name;
+    private int age;
+    private String studentId;
+    private List<String> course;
+
+    Student(String name, int age, String studentId){
+      if (validateStudentId(studentId) && validateName(name) && validateAge(age)) {
+        this.name = name;
+        this.age = age;
+        this.studentId = studentId;
+        course = new ArrayList<String>();
+      }
+    }
+
+    public void enrollCourse(String courses){
+        if(validateCourse(courses)){
+            if (!courses.contains(courses) && validateCourse(courses)) {
+                course.add(courses); 
+                System.out.println("Student is enrolled to " + courses + " Successfully!!");
+            }else{
+                System.err.println("Student is already enrolled to the Course" + courses);
+            }
+        }
+       
+    }
+
+
+    public void printStudentInfo(){
+        System.out.println("Name : " + name);
+        System.out.println("Age : " + age);
+        System.out.println("Student ID : " + studentId);
+        System.out.println("List : " + course);
+    }
+    public String toString(){
+        return "Name: " + name+ " Age: " + age + " Student Id: " + studentId + " Course: "+ course; 
+    }
+
+    public String getName() {
+		return name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public String getStudentId() {
+		return studentId;
+	}
+
+	public List<String> getCourses() {
+		return course;
+	}
+
+
+    public boolean validateStudentId(String studentId){
+        String studentRegex = "S-\\d+$";
+        Pattern studPattern = Pattern.compile(studentRegex);
+        Matcher studenMatcher = studPattern.matcher(studentRegex);
+        if(studenMatcher.matches()){
+            return true;
+        }else{
+            System.err.println("Invalid!! Student ID please use the give format S-123");
+            return false;
+        }
+    }
+
+    public boolean validateName(String name){
+        String nameRegex = "^[a-zA-Z\\s]+$";
+        Pattern namePattern = Pattern.compile(nameRegex);
+        Matcher nameMatcher = namePattern.matcher(nameRegex);
+        if(nameMatcher.matches()){
+            return true;
+        }else{
+            System.err.println("Invalid Name !! Please Enter Alphabate");
+            return false;
+        }
+    }
+    public boolean validateAge(int age){
+        if(age>=19 && age<=35){
+            return true;
+        }else{
+            System.err.println("Invalid!! Age Your Age Must be between 19 to 35");
+            return false;
+        }
+    }
+
+    public boolean validateCourse(String courses){
+        if (courses.equalsIgnoreCase("Java") && courses.equalsIgnoreCase("Python") && courses.equalsIgnoreCase("DevOps")) {
+            return true;
+        }else{
+            System.err.println("Invalid Course!! Please enter the course form the give list [Java, DSA, DevOps]");
+            return false;
+        }
+    }
+
+}
